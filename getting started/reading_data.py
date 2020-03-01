611e393c4124250512e5949f76439f275f311a12
Zorbey Canturk <4762427+thezorb@users.noreply.github.com>
2020-03-01 14:47:03 -0500

    Create reading_data.py

---
+++ getting started/reading_data.py
@@ -0,0 +1,12 @@
+#Reading CSV files
+#There are a ton of options for the read_csv function that can simplify preprocessing of data. Nobody want to waste time cleaning data, so see if you can knock it out when import the initial file.
+
+df = pd.read_csv('pizza.csv')
+
+#Need to parse dates? Just pass in the corresponding column name(s).
+
+df = pd.read_csv('pizza.csv', parse_dates=['dates'])
+
+#Only need a few specific columns?
+
+df = pd.read_csv('pizza.csv', usecols=['foo', 'bar'])
